5ec56b766287a2622282acbed955ffc6c0f7f9f4
utensil <utensilcandel@gmail.com>
2011-07-12 22:45:51 +0800

    rails new utensil-testbed git init git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UtensilTestbed::Application.config.secret_token = 'ee3032db8d5dc1239b3bcd860bf153c5819c8eedee60070b0c699b3d66d35e45c05e6d233079628d686f572f170bc20f5e0aa366f6ab2374a28d02efbd2268b8'
